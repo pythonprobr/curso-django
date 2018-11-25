ff4f04d3a69cd9e38e17925a581f5cbcff6aa0c7
renzon <renzon@gmail.com>
2018-11-25 12:30:05 -0200

    Desacoplada configuração SECRET_KEY
    close #20

--- pypro/settings.py
+++ pypro/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uo$56_d$ui#ifd!#n+ne58*l*fmoi*r46j0!^$lowv)kw!44su'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
