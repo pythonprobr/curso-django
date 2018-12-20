6179d485342523dcca196c5ae511cb9aad46f709
renzon <renzon@gmail.com>
2018-12-20 14:16:12 -0200

    Alterado acesso para privado ao  S3, feito update de dependencias.
    close #81

--- pypro/settings.py
+++ pypro/settings.py
@@ -151,11 +151,11 @@ if AWS_ACCESS_KEY_ID:
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
-    AWS_QUERYSTRING_AUTH = False
+    AWS_QUERYSTRING_AUTH = True
+    AWS_S3_CUSTOM_DOMAIN = None
 
     COLLECTFAST_ENABLED = True
-
-    AWS_DEFAULT_ACL = 'public-read'
+    AWS_DEFAULT_ACL = 'private'
 
     # Static Assets
     # ------------------------------------------------------------------------------
