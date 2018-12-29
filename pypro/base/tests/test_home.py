51551c6bfee1f80c9bf267b0c04f463854542ef4
renzon <renzon@gmail.com>
2018-12-28 22:58:32 -0200

    Incluida função assert_contains

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,6 +1,13 @@
 from django.test import Client
 
+from pypro.django_assertions import assert_contains
+
 
 def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200
+
+
+def test_title(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<title>Python Pro</title>')
