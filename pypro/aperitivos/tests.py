d9752cacea1d8a1f581660538ad1e60129aed27d
renzon <renzon@gmail.com>
2019-01-17 21:28:54 -0200

    Incorporado primeiro vídeo do vimeo de forma manual
    close #123

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse
 
+from pypro.django_assertions import assert_contains
+
 
 @pytest.fixture
 def resp(client):
@@ -9,3 +11,11 @@ def resp(client):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_titulo_video(resp):
+    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/251224475"')
