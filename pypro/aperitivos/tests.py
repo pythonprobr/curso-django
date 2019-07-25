103bcb396657f75918751d90ee743efeb79ad0a7
renzon <renzon@gmail.com>
2019-07-25 12:24:33 -0300

    Implementada Herança de Templates Home e Video
    close #417

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -14,7 +14,7 @@ def test_status_code(resp):
 
 
 def test_titulo_video(resp):
-    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+    assert_contains(resp, 'Video Aperitivo: Motivação')
 
 
 def test_conteudo_video(resp):
