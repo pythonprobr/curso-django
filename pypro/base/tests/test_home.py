b6bcd0a5b93f139485090a59a8f7867f640ba1f5
renzon <renzon@gmail.com>
2019-07-25 12:24:33 -0300

    Implementada Herança de Templates Home e Video
    close #417

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -15,7 +15,7 @@ def test_status_code(resp):
 
 
 def test_title(resp):
-    assert_contains(resp, '<title>Python Pro</title>')
+    assert_contains(resp, '<title>Python Pro - Home</title>')
 
 
 def test_home_link(resp):
