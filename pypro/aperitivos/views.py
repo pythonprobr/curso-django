7b4f85f252b5ef0b94a202b0be1ab3d2145ed6b7
renzon <renzon@gmail.com>
2019-08-02 15:30:00 -0300

    Utilizado filtro default no titulo do video
    close #448

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def video(request, slug):
     videos = {
         'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 251224475},
-        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 251497668},
+        'instalacao-windows': {'titulo': '', 'vimeo_id': 251497668},
     }
     video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
