2490714fac25966a51e443a875dc2d36608e4739
renzon <renzon@gmail.com>
2019-08-02 11:25:37 -0300

    Extraido contexto de template de video
    close #442

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 251224475}
+    return render(request, 'aperitivos/video.html', context={'video': video})
