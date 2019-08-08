5e78a87ab9c2dc55e65cd0681ff7702e86cfc8c9
renzon <renzon@gmail.com>
2019-08-08 17:44:46 -0300

    Criada página de indices de vídeos aperitivos
    close #459

---
+++ pypro/aperitivos/tests/tests_indice.py
@@ -0,0 +1,28 @@
+import pytest
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:indice'))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+@pytest.mark.parametrize(
+    'titulo',
+    [
+        'Video Aperitivo: Motivação',
+        'Instalação Windows'
+    ]
+)
+def test_titulo_video(resp, titulo):
+    assert_contains(resp, titulo)
+#
+#
+# def test_conteudo_video(resp):
+#     assert_contains(resp, '<iframe src="https://player.vimeo.com/video/251224475"')
--- pypro/aperitivos/urls.py
+++ pypro/aperitivos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from pypro.aperitivos.views import video
+from pypro.aperitivos.views import video, indice
 
 app_name = 'aperitivos'
 urlpatterns = [
     path('<slug:slug>', video, name='video'),
+    path('', indice, name='indice'),
 ]
