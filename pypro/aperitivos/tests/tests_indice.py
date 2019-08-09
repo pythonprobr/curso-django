c36dcc50ff3211b57b6f7767617733d023c07718
renzon <renzon@gmail.com>
2019-08-09 08:44:00 -0300

    Criação e acesso de get_absolute_url para video
    close #462

--- pypro/aperitivos/tests/tests_indice.py
+++ pypro/aperitivos/tests/tests_indice.py
@@ -22,7 +22,15 @@ def test_status_code(resp):
 )
 def test_titulo_video(resp, titulo):
     assert_contains(resp, titulo)
-#
-#
-# def test_conteudo_video(resp):
-#     assert_contains(resp, '<iframe src="https://player.vimeo.com/video/251224475"')
+
+
+@pytest.mark.parametrize(
+    'slug',
+    [
+        'motivacao',
+        'instalacao-windows'
+    ]
+)
+def test_link_video(resp, slug):
+    video_link = reverse('aperitivos:video', args=(slug,))
+    assert_contains(resp, f'href="{video_link}"')
