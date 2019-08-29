48511bb088d035d67331d827c25bf2fe4478553f
renzon <renzon@gmail.com>
2019-08-29 17:07:26 -0300

    Implementdo indice com acesso a banco de dados
    Parcial de #498

--- pypro/aperitivos/tests/test_indice.py
+++ pypro/aperitivos/tests/test_indice.py
@@ -1,11 +1,18 @@
 import pytest
 from django.urls import reverse
+from model_mommy import mommy
 
+from pypro.aperitivos.models import Video
 from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture
-def resp(client):
+def videos(db):
+    return mommy.make(Video, 3)
+
+
+@pytest.fixture
+def resp(client, videos):
     return client.get(reverse('aperitivos:indice'))
 
 
@@ -13,24 +20,12 @@ def test_status_code(resp):
     assert resp.status_code == 200
 
 
-@pytest.mark.parametrize(
-    'titulo',
-    [
-        'Video Aperitivo: Motivação',
-        'Instalação Windows'
-    ]
-)
-def test_titulo_video(resp, titulo):
-    assert_contains(resp, titulo)
+def test_titulo_video(resp, videos):
+    for video in videos:
+        assert_contains(resp, video.titulo)
 
 
-@pytest.mark.parametrize(
-    'slug',
-    [
-        'motivacao',
-        'instalacao-windows'
-    ]
-)
-def test_link_video(resp, slug):
-    video_link = reverse('aperitivos:video', args=(slug,))
-    assert_contains(resp, f'href="{video_link}"')
+def test_link_video(resp, videos):
+    for video in videos:
+        video_link = reverse('aperitivos:video', args=(video.slug,))
+        assert_contains(resp, f'href="{video_link}"')
