028c9358ec9cc38af0f3f6ca58700bc8cfa2f700
renzon <renzon@gmail.com>
2019-08-29 17:07:26 -0300

    Implementdo indice com acesso a banco de dados
    Parcial de #498

--- pypro/aperitivos/models.py
+++ pypro/aperitivos/models.py
@@ -6,6 +6,7 @@ class Video(models.Model):
     slug = models.CharField(max_length=32)
     titulo = models.CharField(max_length=32)
     vimeo_id = models.CharField(max_length=32)
+    creation = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
         return reverse('aperitivos:video', args=(self.slug,))
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,15 +2,9 @@ from django.shortcuts import get_object_or_404, render
 
 from pypro.aperitivos.models import Video
 
-videos = [
-    Video(slug='motivacao', titulo='Video Aperitivo: Motivação', vimeo_id='251224475'),
-    Video(slug='instalacao-windows', titulo='Instalação Windows', vimeo_id='251497668'),
-]
-
-videos_dct = {v.slug: v for v in videos}
-
 
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
