d6f7d46a34671fc836d6eb78bd37d98ba79e424b
renzon <renzon@gmail.com>
2019-09-13 15:04:04 -0300

    Acrescentado slug ao Módulo
    close #525

--- pypro/modulos/admin.py
+++ pypro/modulos/admin.py
@@ -7,3 +7,4 @@ from pypro.modulos.models import Modulo
 @admin.register(Modulo)
 class ModuloAdmin(OrderedModelAdmin):
     list_display = ('titulo', 'publico', 'move_up_down_links')
+    prepopulated_fields = {'slug': ('titulo',)}
--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from ordered_model.models import OrderedModel
 
 
@@ -6,9 +7,13 @@ class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
     publico = models.TextField()
     descricao = models.TextField()
+    slug = models.SlugField(unique=True)
 
     class Meta(OrderedModel.Meta):
         pass
 
     def __str__(self):
         return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('modulos:detalhe', kwargs={'slug': self.slug})
