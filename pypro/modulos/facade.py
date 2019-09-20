a6bd429b963aba7dbc0f728ff46f9d4c5153c7a3
renzon <renzon@gmail.com>
2019-09-20 15:13:17 -0300

    Criada página de detalhes de módulo
    close #540

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -10,3 +10,7 @@ def listar_modulos_ordenados() -> List[Modulo]:
     """
 
     return list(Modulo.objects.order_by('order').all())
+
+
+def encontrar_modulo(slug: str) -> Modulo:
+    return Modulo.objects.get(slug=slug)
