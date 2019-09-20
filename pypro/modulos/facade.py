a06a0690e0fe71d2d56e846492896163608e69e3
renzon <renzon@gmail.com>
2019-09-20 17:58:17 -0300

    Criada página com aulas de módulos
    close #559

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -14,3 +14,7 @@ def listar_modulos_ordenados() -> List[Modulo]:
 
 def encontrar_modulo(slug: str) -> Modulo:
     return Modulo.objects.get(slug=slug)
+
+
+def listar_aulas_de_modulo_ordenadas(modulo: Modulo):
+    return list(modulo.aula_set.order_by('order').all())
--- pypro/modulos/tests/test_modulo_detalhe.py
+++ pypro/modulos/tests/test_modulo_detalhe.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from model_mommy import mommy
 
 from pypro.django_assertions import assert_contains
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Aula, Modulo
 
 
 @pytest.fixture
@@ -12,7 +12,12 @@ def modulo(db):
 
 
 @pytest.fixture
-def resp(client, modulo):
+def aulas(modulo):
+    return mommy.make(Aula, 3, modulo=modulo)
+
+
+@pytest.fixture
+def resp(client, modulo, aulas):
     resp = client.get(reverse('modulos:detalhe', kwargs={'slug': modulo.slug}))
     return resp
 
@@ -27,3 +32,8 @@ def test_descricao(resp, modulo: Modulo):
 
 def test_publico(resp, modulo: Modulo):
     assert_contains(resp, modulo.publico)
+
+
+def test_aulas_titulos(resp, aulas):
+    for aula in aulas:
+        assert_contains(resp, aula.titulo)
