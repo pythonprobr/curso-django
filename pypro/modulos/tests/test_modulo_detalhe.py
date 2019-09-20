fdfe55aceb6de6e7bcdd694f5f79feff356245bb
renzon <renzon@gmail.com>
2019-09-20 15:13:17 -0300

    Criada página de detalhes de módulo
    close #540

---
+++ pypro/modulos/tests/test_modulo_detalhe.py
@@ -0,0 +1,29 @@
+import pytest
+from django.urls import reverse
+from model_mommy import mommy
+
+from pypro.django_assertions import assert_contains
+from pypro.modulos.models import Modulo
+
+
+@pytest.fixture
+def modulo(db):
+    return mommy.make(Modulo)
+
+
+@pytest.fixture
+def resp(client, modulo):
+    resp = client.get(reverse('modulos:detalhe', kwargs={'slug': modulo.slug}))
+    return resp
+
+
+def test_titulo(resp, modulo: Modulo):
+    assert_contains(resp, modulo.titulo)
+
+
+def test_descricao(resp, modulo: Modulo):
+    assert_contains(resp, modulo.descricao)
+
+
+def test_publico(resp, modulo: Modulo):
+    assert_contains(resp, modulo.publico)
--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -1,3 +1,8 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+
+from pypro.modulos import facade
+
+
 def detalhe(request, slug):
-    pass
+    modulo = facade.encontrar_modulo(slug)
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo})
