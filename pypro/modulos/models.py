698770d7d9a12d148bdb224dab7dc582cba2ffec
renzon <renzon@gmail.com>
2019-09-20 17:58:17 -0300

    Criada página com aulas de módulos
    close #559

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -17,3 +17,19 @@ class Modulo(OrderedModel):
 
     def get_absolute_url(self):
         return reverse('modulos:detalhe', kwargs={'slug': self.slug})
+
+
+class Aula(OrderedModel):
+    titulo = models.CharField(max_length=64)
+    slug = models.SlugField(unique=True)
+    modulo = models.ForeignKey('Modulo', on_delete=models.PROTECT)
+    order_with_respect_to = 'modulo'
+
+    class Meta(OrderedModel.Meta):
+        pass
+
+    def __str__(self):
+        return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('modulos:aula', kwargs={'slug': self.slug})
--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -5,4 +5,5 @@ from pypro.modulos import facade
 
 def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
-    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo})
+    aulas = facade.listar_aulas_de_modulo_ordenadas(modulo)
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
