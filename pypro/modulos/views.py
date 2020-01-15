6079f29bf7591698ed81fa3e763b19196efc0ce9
renzon <renzon@gmail.com>
2020-01-15 09:42:20 -0300

    Criados links para aulas
    close #787

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -7,3 +7,7 @@ def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
     aulas = facade.listar_aulas_de_modulo_ordenadas(modulo)
     return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
+
+
+def aula(request, slug):
+    pass
