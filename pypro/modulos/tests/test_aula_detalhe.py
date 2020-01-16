da584e35b8ed6cf349c2e1849a2518e865491dd9
renzon <renzon@gmail.com>
2020-01-16 14:55:55 -0300

    Implementado Breacrumb ineficiente
    hold #798

--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -27,4 +27,8 @@ def test_titulo(resp, aula: Aula):
 
 
 def test_vimeo(resp, aula: Aula):
-    assert_contains(resp, f'src="https://player.vimeo.com/video/{ aula.vimeo_id }"')
+    assert_contains(resp, f'src="https://player.vimeo.com/video/{aula.vimeo_id}"')
+
+
+def test_modulo_breadcrumb(resp, modulo: Modulo):
+    assert_contains(resp, f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">{modulo.titulo}</a></li>')
