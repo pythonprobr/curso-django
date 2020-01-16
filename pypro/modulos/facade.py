6f5f9dfcb2f4ca86c977f1ccb7bdf6d2aa276525
renzon <renzon@gmail.com>
2020-01-16 15:44:54 -0300

    Implementado Breacrumb eficiente
    hold #798

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -21,4 +21,4 @@ def listar_aulas_de_modulo_ordenadas(modulo: Modulo):
 
 
 def encontrar_aula(slug):
-    return Aula.objects.get(slug=slug)
+    return Aula.objects.select_related('modulo').get(slug=slug)
