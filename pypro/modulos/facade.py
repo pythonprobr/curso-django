8aa64a470e8caea277d7d6484b8c94b0f91fd04f
renzon <renzon@gmail.com>
2020-01-29 09:24:50 -0300

    Indice implementado com objeto Prefech
    close #789

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from django.db.models import Prefetch
+
 from pypro.modulos.models import Aula, Modulo
 
 
@@ -25,4 +27,7 @@ def encontrar_aula(slug):
 
 
 def listar_modulos_com_aulas():
-    return Modulo.objects.order_by('order').prefetch_related('aula_set').all()
+    aulas_ordenadas = Aula.objects.order_by('order')
+    return Modulo.objects.order_by('order').prefetch_related(
+        Prefetch('aula_set', queryset=aulas_ordenadas, to_attr='aulas')
+    ).all()
