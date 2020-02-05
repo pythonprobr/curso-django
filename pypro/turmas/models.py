fb397581d95f60e46ca684b92a0f58a2c8cc96f1
renzon <renzon@gmail.com>
2020-02-05 10:43:35 -0300

    Criada tabela intermediária de Matrícula
    parte de #830

--- pypro/turmas/models.py
+++ pypro/turmas/models.py
@@ -9,3 +9,10 @@ class Turma(models.Model):
     slug = models.SlugField(max_length=64)
     inicio = models.DateField()
     fim = models.DateField()
+    alunos = models.ManyToManyField(get_user_model(), through='Matricula')
+
+
+class Matricula(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
