d7811cbc5a10ca8e5da28a203e4e2be4ce25e246
renzon <renzon@gmail.com>
2020-02-12 17:11:33 -0300

    Criado Formulário Simples de Login
    Parte de #858

--- pypro/urls.py
+++ pypro/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contas/', include('django.contrib.auth.urls')),
     path('', include('pypro.base.urls')),
     path('aperitivos/', include('pypro.aperitivos.urls')),
     path('modulos/', include('pypro.modulos.urls')),
