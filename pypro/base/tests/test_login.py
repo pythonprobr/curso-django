d4a3e85cdee7cc67591313769d745e090f5d7696
renzon <renzon@gmail.com>
2020-02-13 14:04:38 -0300

    Cliente redirecionado para /modulos/ após login
    Parte de #858

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from model_mommy import mommy
 
 
 @pytest.fixture
@@ -9,3 +10,23 @@ def resp(client, db):
 
 def test_login_form_page(resp):
     assert resp.status_code == 200
+
+
+@pytest.fixture
+def usuario(db, django_user_model):
+    usuario_modelo = mommy.make(django_user_model)
+    senha = 'senha'
+    usuario_modelo.set_password(senha)
+    usuario_modelo.save()
+    usuario_modelo.senha_plana = senha
+    return usuario_modelo
+
+
+@pytest.fixture
+def resp_post(client, usuario):
+    return client.post(reverse('login'), {'username': usuario.email, 'password': usuario.senha_plana})
+
+
+def test_login_redirect(resp_post):
+    assert resp_post.status_code == 302
+    assert resp_post.url == reverse('modulos:indice')
