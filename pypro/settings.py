93a080c2cbca5145097b13b81e2ac8f4a528747f
renzon <renzon@gmail.com>
2020-05-27 16:13:24 -0300

    Configurados parâmetros de envio de email
    parte de #1070

--- pypro/settings.py
+++ pypro/settings.py
@@ -87,6 +87,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pypro.wsgi.application'
 
+# Configuração de envio de Email
+
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+
+
 # Configuração Django Debug Tollbar
 
 INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
