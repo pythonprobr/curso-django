e486b9133123f56911ce1ada59fd91f10374452c
carloslira22 <carlos.lira.cv@gmail.com>
2020-06-03 20:42:04 -0300

    Criados arquivos para deploy no Heroku

--- pypro/settings.py
+++ pypro/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+"""ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())"""
+
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'base.User'
 
