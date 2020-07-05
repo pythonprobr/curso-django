4ac09f00459cc2bf5f1f545689e3b8966b0b2cdd
carloslira22 <carlos.lira.cv@gmail.com>
2020-07-05 20:00:37 -0300

    Atualizando arquivos

--- manage.py
+++ manage.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
-def main():
+if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pypro.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -15,7 +13,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
