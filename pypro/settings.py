29eec0d4f3b1a1f31318fd64185213fbc6bafc0e
carloslira22 <carlos.lira.cv@gmail.com>
2020-07-15 19:37:03 -0300

    Atualização de settings.py

--- pypro/settings.py
+++ pypro/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'glm)squ7(jagncv!%fea_qfw-gcrvgj43h(uhmz59h5nps0!u&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
