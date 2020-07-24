377a7b7a29d8c51a896e0b25c05b9641ba91f273
carloslira22 <carlos.lira.cv@gmail.com>
2020-07-23 21:31:38 -0300

    Atualização de settings.py e Pipfile.lock

--- pypro/settings.py
+++ pypro/settings.py
@@ -80,9 +80,7 @@ default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 parse_database = partial(dj_database_url.parse, conn_max_age=600)
 
-DATABASES = {
-'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
-}
+DATABASES = dict(default=config('DATABASE_URL', default=default_db_url, cast=parse_database))
 
 
 # Password validation
