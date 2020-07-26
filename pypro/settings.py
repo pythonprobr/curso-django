6594945abbe104041328b31377d4cd7849c118f0
carloslira22 <carlos.lira.cv@gmail.com>
2020-07-26 15:13:02 -0300

    Atualização de settings.py

--- pypro/settings.py
+++ pypro/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# Configuração de ambiente de desenvolvimento
 
 STATIC_URL = '/static/'
 
