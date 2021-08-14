f32237fba4f3977b2af904b11a312632b6883e9d
Jarlan <silvajarlan@gmail.com>
2021-08-14 16:56:42 -0300

    Começando django novamente

--- pypro/aperitivos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from pypro.aperitivos.views import video, indice
-
-app_name = 'aperitivos'
-urlpatterns = [
-    path('<slug:slug>', video, name='video'),
-    path('', indice, name='indice'),
-]
