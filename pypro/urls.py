5ba04f6d9064c7d22a8abbcd7b4828705a560073
Rudie23 <ruandiego100@gmail.com>
2022-03-09 17:44:34 -0300

    Criada app base!

--- pypro/urls.py
+++ pypro/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pypro.base.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
