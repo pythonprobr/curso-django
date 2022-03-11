78926a61f498ae1d8fa3f1a109820348d520e578
Rudie23 <ruandiego100@gmail.com>
2022-03-11 18:34:09 -0300

    Timezone e Language

--- pypro/settings.py
+++ pypro/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
