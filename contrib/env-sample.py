b21a65e7896e65fda946e25e7b5c8b0215327226
Rudie23 <ruandiego100@gmail.com>
2022-03-12 18:19:25 -0300

    Configurando a Lib django_s3_folder_storage. Colocado no env-smaple

--- contrib/env-sample.py
+++ contrib/env-sample.py
@@ -1,4 +1,8 @@
-DEBUG = False
+DEBUG = 'False'
 SECRET_KEY = 'defina sua chave secreta'
 ALLOWED_HOSTS = 'localhost, 127.0.0.1'
 DATABASE_URL = 'postgres://postgres:postgres@localhost/testdb'
+# COnfigurações do AWS
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = ''
+AWS_S3_OBJECT_PARAMETERS = ''
