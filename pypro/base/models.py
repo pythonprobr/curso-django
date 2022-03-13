51bf95b4f1caae8522b2265dcbf9540da0fef56e
Rudie23 <ruandiego100@gmail.com>
2022-03-13 16:28:29 -0300

    Comando Makemigrations e Alias mng.

--- pypro/base/models.py
+++ pypro/base/models.py
@@ -101,7 +101,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["email"]
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = _("user")
