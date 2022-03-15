9064355a3367ba2229ba5569ec6023e925be59df
Rudie23 <ruandiego100@gmail.com>
2022-03-14 21:39:39 -0300

    Django Debug Toolbar

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
+    return HttpResponse('<html><body>Hello Django</body></html>')
