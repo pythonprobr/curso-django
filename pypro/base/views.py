d5fdd31dd7a13b99dc59fc1a1acde840a8348caa
Rudie23 <ruandiego100@gmail.com>
2022-03-16 15:30:19 -0300

    Tive muitos problemas com o arquivo home.html

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
-
-
 # Create your views here.
+from django.shortcuts import render
+
 
 def home(request):
-    return HttpResponse('<html><body>Hello Django</body></html>')
+    return render(request, 'base/home.html')
