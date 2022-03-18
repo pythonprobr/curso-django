b31bae42861187f757b2108926e072fda69fa86e
Rudie23 <ruandiego100@gmail.com>
2022-03-18 20:46:00 -0300

    App criada

---
+++ pypro/base/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def home(request):
+    return HttpResponse('Hello, Django!')
--- pypro/urls.py
+++ pypro/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from pypro.base.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
