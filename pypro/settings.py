e68517a773e33ec487c4b35b5b70c2f8a94aeda9
Rudie23 <ruandiego100@gmail.com>
2022-03-19 14:07:00 -0300

    Static files configurados no arquivo settings.py

--- pypro/settings.py
+++ pypro/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
