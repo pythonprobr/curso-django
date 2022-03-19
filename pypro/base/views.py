67b0ac237fd4b821b38ebe1d4fb4754eaa899c52
Rudie23 <ruandiego100@gmail.com>
2022-03-19 20:54:40 -0300

    Django Debug Toolbar

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Hello, Django!')
+    return HttpResponse('<html><body>Hello, Django!</body></html>')
--- pypro/settings.py
+++ pypro/settings.py
@@ -77,6 +77,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pypro.wsgi.application'
 
+# Configuração Django Debug Tollbar
+
+INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
